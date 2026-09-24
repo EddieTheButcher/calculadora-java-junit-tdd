33320590dac1455ac8fdfbbfb3552893a3d72eb9
Eduardo Iglesias <duiglesias2003@gmail.com>
2026-09-24 13:15:45 -0300

    Adiciona exercício de calculadora com JUnit 5

---
+++ src/CalculadoraTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class CalculadoraTest {
+
+    @Test
+    public void testSoma() {
+        Calculadora calc = new Calculadora();
+
+        assertEquals(5, calc.soma(2, 3));
+    }
+}
